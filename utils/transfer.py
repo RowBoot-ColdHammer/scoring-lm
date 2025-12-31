166539941960927df809430078e5173a43e7a531
NetBiosP139 <65152247+norv139@users.noreply.github.com>
2025-12-31 12:49:40 +0100

    update transfer

--- utils/transfer.py
+++ utils/transfer.py
@@ -14,42 +14,25 @@ sqlite_db = os.path.join(db_dir, 'test.db')
 data_file = os.path.join(csv_dir, 'test_input_data.csv')
 
 
-# conn = sqlite3.connect(sqlite_db)
-# cursor = conn.cursor()
-# cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS customers (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         age INTEGER,
-#         income REAL,
-#         education BOOLEAN,
-#         work BOOLEAN,
-#         car BOOLEAN,
-#         "default" INTEGER
-#     )
-# ''')
-# conn.commit()
-# conn.close()
+conn = sqlite3.connect(sqlite_db)
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS customers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        age INTEGER,
+        income REAL,
+        education BOOLEAN,
+        work BOOLEAN,
+        car BOOLEAN,
+        "default" INTEGER
+    )
+''')
+conn.commit()
+conn.close()
 
 
 data = pd.read_csv(data_file)
 
 conn = sqlite3.connect(sqlite_db)
 data.to_sql('customers', conn, if_exists='replace', index=False)
-conn.close() 
-
-# try:
-#     cursor = conn.cursor()
-#     cursor.execute('''
-#         INSERT INTO customers (age, income, education, work, car, "default")
-#         VALUES (?, ?, ?, ?, ?, ?)
-#     ''', (
-#         request['age'],
-#         request['income'],
-#         request['education'],
-#         request['work'],
-#         request['car'],
-#         default_pred
-#     ))
-#     conn.commit()
-# finally:
-#     conn.close()
+conn.close()
